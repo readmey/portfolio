65a1fe363ae58594c1fc4860398934780b7b2920
My Trang Hong <mytrang.hong@prodyna.com>
2020-05-17 19:03:59 +0200

    init react app

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./App.scss";
+
+import Portfolio from "./pages/portfolio";
+
+function App() {
+  return (
+    <div className="App__wrapper">
+      <header className="App__header"></header>
+      <Portfolio />
+    </div>
+  );
+}
+
+export default App;
