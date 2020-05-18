dbb465d45c798b1219e5e463389f34d85ed0dc5c
My Trang Hong <mytrang.hong@prodyna.com>
2020-05-18 22:19:15 +0200

    replace favicon

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
 import React from "react";
 import "./App.scss";
 
+import data from "./data/data.json";
+
 import Portfolio from "./pages/portfolio";
 import PositionTitle from "./components/PositionTitle";
+import ContactDetails from "./components/ContactDetails";
 
+console.log(data);
 function App() {
   return (
     <div className="App">
-      <header></header>
+      <header>
+        <ContactDetails contact={data.contactDetails} />
+      </header>
       <PositionTitle title="MY TRANG HONG" />
-      <Portfolio />
+      <Portfolio data={data} />
     </div>
   );
 }
--- src/pages/portfolio.js
+++ src/pages/portfolio.js
@@ -1,7 +1,33 @@
 import React from "react";
+import styled from "styled-components";
 
-const Portfolio = () => {
-  return <div className="portfolio"></div>;
+import SkillScale from "../components/SkillScale";
+
+const GridFlex = styled.div`
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  justify-content: space-between;
+`;
+
+const ColumnFlex = styled.div`
+  width: 45%;
+`;
+
+const Portfolio = (props) => {
+  const { data } = props;
+  return (
+    <div className="portfolio">
+      <GridFlex>
+        <ColumnFlex>
+          <SkillScale type="bar" skills={data.languages} />
+        </ColumnFlex>
+        <ColumnFlex>
+          <SkillScale type="bubbles" skills={data.skillsMatrix} />
+        </ColumnFlex>
+      </GridFlex>
+    </div>
+  );
 };
 
 export default Portfolio;
