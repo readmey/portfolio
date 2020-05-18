3b6d3356ff4290f57cf5b4d5a72808889e222e1f
readmey <mytrang.hong@hotmail.de>
2020-05-18 09:08:18 +0200

    add title component

--- src/pages/portfolio.js
+++ src/pages/portfolio.js
@@ -1,11 +1,7 @@
 import React from "react";
 
 const Portfolio = () => {
-  return (
-    <div className="portfolio">
-      <h1>Portfolio</h1>
-    </div>
-  );
+  return <div className="portfolio"></div>;
 };
 
 export default Portfolio;
