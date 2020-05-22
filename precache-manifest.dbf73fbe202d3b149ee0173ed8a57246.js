fa5e4b6a4b55237ec404aa0b385a294888ac0be7
My Trang Hong <mytrang.hong@prodyna.com>
2020-05-22 12:27:16 +0200

    Updates

---
+++ precache-manifest.dbf73fbe202d3b149ee0173ed8a57246.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db60945bc364c7f5ced8d3c47404cba0",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "425ef0bf0bc286d56bf2",
+    "url": "/portfolio/static/js/2.f6275814.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/portfolio/static/js/2.f6275814.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4d978b19e1f9737c5afc",
+    "url": "/portfolio/static/js/main.1d15fdee.chunk.js"
+  },
+  {
+    "revision": "e5284505c87d0b24b753",
+    "url": "/portfolio/static/js/runtime-main.f4f2abbf.js"
+  },
+  {
+    "revision": "541daaf9c4d9e4472fbff2b9e2cbed5f",
+    "url": "/portfolio/static/media/Character.541daaf9.svg"
+  },
+  {
+    "revision": "c352e7b670254267d30d47bc3a52b3b2",
+    "url": "/portfolio/static/media/D-DIN-Bold.c352e7b6.otf"
+  },
+  {
+    "revision": "d68d8669232a9e8215831889eba83db1",
+    "url": "/portfolio/static/media/D-DIN.d68d8669.otf"
+  },
+  {
+    "revision": "19310716928318c9e1e4813aa9ede764",
+    "url": "/portfolio/static/media/D-DINCondensed-Bold.19310716.otf"
+  },
+  {
+    "revision": "2c8b34e4210d18b1d3b8632cf6fa0716",
+    "url": "/portfolio/static/media/D-DINCondensed.2c8b34e4.otf"
+  },
+  {
+    "revision": "9a1ca66f1513b2360d567ba651ac26b7",
+    "url": "/portfolio/static/media/certification-icon.9a1ca66f.svg"
+  },
+  {
+    "revision": "3592ebd0a2b5dbe99fed8a33401c0126",
+    "url": "/portfolio/static/media/education-icon.3592ebd0.svg"
+  },
+  {
+    "revision": "8f8edc5fb64b9bd436834dc926ac992c",
+    "url": "/portfolio/static/media/github.8f8edc5f.svg"
+  },
+  {
+    "revision": "799008eef05834161796ef8027cfe0aa",
+    "url": "/portfolio/static/media/language-icon.799008ee.svg"
+  },
+  {
+    "revision": "007eb41ac7dadcaf97d565a787bf8afc",
+    "url": "/portfolio/static/media/linkedin.007eb41a.svg"
+  },
+  {
+    "revision": "2ce0610908ef47615cd1a13609688911",
+    "url": "/portfolio/static/media/projects-icon.2ce06109.svg"
+  },
+  {
+    "revision": "13d9e0a0d3947bb023c8b3ec37360019",
+    "url": "/portfolio/static/media/skills-icon.13d9e0a0.svg"
+  },
+  {
+    "revision": "6be881018891e4ff4d128a202a2371a1",
+    "url": "/portfolio/static/media/work-experience-icon.6be88101.svg"
+  }
+]);
