4dde999786af74505f4112b5f6fe32dbbd176a24
My Trang Hong <mytrang.hong@prodyna.com>
2020-12-21 12:34:06 +0100

    add timeline component

--- src/pages/portfolio.js
+++ src/pages/portfolio.js
@@ -1,39 +1,99 @@
 import React from "react";
 import styled from "styled-components";
-
+import theme from "../styles/theme";
 import SkillScale from "../components/SkillScale";
 import CustomTitle from "../components/CustomTitle";
-
+import EducationIcon from "../assets/icons/education-icon.svg";
+import AchievementIcon from "../assets/icons/certification-icon.svg";
 import LanguageIcon from "../assets/icons/language-icon.svg";
+import WorkIcon from "../assets/icons/work-experience-icon.svg";
+import ProjectsIcon from "../assets/icons/projects-icon.svg";
 import SkillsIcon from "../assets/icons/skills-icon.svg";
+import Timeline from "../components/Timeline";
+import projects from "../data/projects.json";
 
 const GridFlex = styled.div`
   display: flex;
   flex-direction: row;
   flex-wrap: wrap;
   justify-content: space-between;
+  margin-bottom: 3rem;
+  @media (min-width: ${theme.breakpoints.sm}) {
+    flex-wrap: nowrap;
+  }
 `;
 
 const ColumnFlex = styled.div`
-  width: 45%;
+  width: 100%;
+  @media (min-width: ${theme.breakpoints.sm}) {
+    width: 45%;
+  }
 `;
 
 const Portfolio = (props) => {
   const { data } = props;
-  return (
-    <div className="portfolio">
-      <GridFlex>
-        <ColumnFlex>
-          <CustomTitle title="Languages" icon={LanguageIcon} />
-          <SkillScale type="bar" skills={data.languages} />
-        </ColumnFlex>
-        <ColumnFlex>
-          <CustomTitle title="Skills" icon={SkillsIcon} />
-          <SkillScale type="bubbles" skills={data.skillsMatrix} />
-        </ColumnFlex>
-      </GridFlex>
-    </div>
-  );
+
+  if (data)
+    return (
+      <div className="portfolio">
+        <GridFlex>
+          <ColumnFlex>
+            <CustomTitle title="Education" icon={EducationIcon} />
+            <Timeline list={
+              data.education.map(item => ({
+              date: item.year,
+              title: item.graduation,
+              subTitle: item.studies,
+              description: item.university,
+              subDescription: `final grade: ${item.grade}`,
+            }))} />
+          </ColumnFlex>
+          <ColumnFlex>
+            <CustomTitle title="Achievements" icon={AchievementIcon} />
+            <Timeline list={
+              data.certification.map(item => ({
+              date: item.year,
+              title: item.title,
+              subTitle: item.organization,
+              description: `License number: ${item.licenseId}`,
+            }))} />
+          </ColumnFlex>
+        </GridFlex>
+        <GridFlex>
+          <ColumnFlex>
+            <CustomTitle title="Work" icon={WorkIcon} />
+            <Timeline list={
+              data.workExperience.map(item => ({
+              date: `${item.startDate} - ${item.endDate}`,
+              title: item.position,
+              subTitle: item.company,
+              description: item.address,
+            }))} />
+          </ColumnFlex>
+          <ColumnFlex>
+            <CustomTitle title="Selection of Projects" icon={ProjectsIcon} />
+            <Timeline list={
+              projects.projects.slice(0, 5).map(item => ({
+              date: `${item.title} - ${item.customer}`,
+              title: item.role,
+              description: item.description,
+              subDescription: item.technologies,
+            }))} />
+          </ColumnFlex>
+        </GridFlex>
+        <GridFlex>
+          <ColumnFlex>
+            <CustomTitle title="Languages" icon={LanguageIcon} />
+            <SkillScale type="bar" skills={data.languages} />
+          </ColumnFlex>
+          <ColumnFlex>
+            <CustomTitle title="Skills" icon={SkillsIcon} />
+            <SkillScale type="bubbles" skills={data.skillsMatrix} />
+          </ColumnFlex>
+        </GridFlex>
+      </div>
+    );
+  return <></>
 };
 
 export default Portfolio;
