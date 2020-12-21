6b255391fd67213b6c8cd09826cbc370451554a9
readmey <mytrang.hong@hotmail.de>
2020-12-21 12:34:06 +0100

    add timeline component

---
+++ src/components/Timeline.js
@@ -0,0 +1,71 @@
+import React from "react";
+import styled from "styled-components";
+import theme from "../styles/theme";
+
+const GridFlex = styled.div`
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  @media (min-width: ${theme.breakpoints.sm}) {
+    flex-wrap: nowrap;
+  }
+`;
+
+const StyledWrapper = styled.div`
+  margin-left: 1rem;
+`;
+
+const StyledBubble = styled.div`
+  display: inline-block;
+  height: 15px;
+  width: 15px;
+  border-radius: 25px;
+  float: right;
+  border: 2px solid ${theme.colors.primary};
+  background-color: white;
+`;
+
+const StyledLine = styled.div`
+  height: 120px;
+  margin-top: 1rem;
+  margin-left: 0.5rem;
+  width: 2px;
+  background-color: ${theme.colors.primary};
+`;
+
+const StyledTitle = styled.div`
+  font-family: "DINCondensed";
+  font-weight: 400;
+  font-size: 18px;
+  display: block;
+`;
+
+const StyledDescription = styled.div`
+  font-family: "DIN";
+`;
+
+const Timeline = (props) => {
+  const { list } = props;
+
+  return (
+    <>
+      { list.map((item, index) => (
+        <GridFlex key={item}>
+          <div>
+            <StyledBubble />
+            {(list.length === 1 || index < list.length - 1)  && <StyledLine />}
+          </div>
+          <StyledWrapper>
+            <StyledTitle>{item.date}</StyledTitle>
+            <StyledTitle>{item.title}</StyledTitle>
+            <StyledDescription>{item.subTitle}</StyledDescription>
+            <StyledDescription>{item.description}</StyledDescription>
+            <StyledDescription>{item.subDescription}</StyledDescription>
+          </StyledWrapper>
+        </GridFlex>
+      ))}
+    </>
+  )
+}
+
+export default Timeline;
