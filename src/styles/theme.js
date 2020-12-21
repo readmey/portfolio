f953d98b5c7e067f66a0ea40979a76ba3c79abd0
readmey <mytrang.hong@hotmail.de>
2020-12-21 12:33:30 +0100

    update theme

--- src/styles/theme.js
+++ src/styles/theme.js
@@ -2,7 +2,7 @@ const theme = {
   colors: {
     primary: "#417505",
     secondary: "#79A546",
-    black: "black",
+    black: "#000000",
   },
   font: {
     baseFontFamily: "DIN, Arial, sans-serif",
@@ -12,6 +12,12 @@ const theme = {
   borders: {
     borderPrimary: "3px solid #417505",
   },
+  breakpoints: {
+    xs: "576px",
+    sm: "768px",
+    md: "992px",
+    lg: "1200px"
+  }
 };
 
 export default theme;
